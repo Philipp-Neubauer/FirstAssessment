b637b068ac3f5278e6566af3d5c3679f3ad6a522
Philipp Neubauer <neubauer.phil@gmail.com>
2014-05-02 09:28:41 +1200

    Working Weibull survival model with cox-snell goodness of fit test

---
+++ helper_functs.R
@@ -0,0 +1,21 @@
+get_coef_chains <- function(model.out,coef.names=NULL,var.names=NULL){
+  
+  if(length(model.out)>1){
+    model.post <- do.call('rbind',model.out)
+  } else {
+    model.post <- unclass(model.out[[1]])
+  }
+  if(length(grep(substitute(coef.names),colnames(model.post)))==1) {
+    chains <- data.frame(model.post[,grep(substitute(coef.names),colnames(model.post))])
+  } else {
+    chains <- model.post[,grep(substitute(coef.names),colnames(model.post))]
+  }
+  if(!is.null(var.names)) colnames(chains) <- var.names
+  if (dim(chains)[2]>1) {
+    chains <- reshape2::melt(chains)[,2:3]
+    colnames(chains) <- c('Parameter','MCMC')
+  } else {
+    colnames(chains) <- 'MCMC'
+  }
+  return(chains)
+}
