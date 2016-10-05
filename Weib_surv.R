345f724e153b9cb8b31d8572288fbac6b3ff480a
Philipp Neubauer <neubauer.phil@gmail.com>
2016-10-04 17:36:06 -0700

    updated analysis - only at species level and with updated habitat from FB

--- Weib_surv.R
+++ Weib_surv.R
@@ -9,7 +9,7 @@ model{
   a.time[i] ~ dweib(tau,mu[i])
   
   # linear predictor
-  log(mu[i]) <- betas[1:n.covs] %*% COVS[i,1:n.covs] + habitat[hab[i]] + taxonomy[tax[i]]
+  mu[i] <- exp(betas[1:n.covs] %*% COVS[i,1:n.covs] + habitat[hab[i]] + classfx[class[i]] + familyfx[family[i]] + orderfx[order[i]])
   
   # cox-snell residuals - use pweib alone for predictions of P(t<T|X) i.e. for some predictor X what is the probability that a stock is assessed within T years
   CS[i] <- -log(1-pweib(ctime[i],tau,mu[i]))
@@ -30,21 +30,43 @@ model{
     hab.eta[j] ~ dnorm(0,hab.prec)
   }
   # half cauchy prior on random effect variance
-  hab.xi ~ dnorm(0,0.04)
+  hab.xi ~ dnorm(0,0.001)
   hab.prec ~ dgamma(0.5,0.5)
   
-  for (l in 1:n.tax){
-    taxonomy[l] <- tax.xi*tax.eta[l]
-    tax.eta[l] ~ dnorm(0,tax.prec)
+  for (l in 1:n.class){
+    classfx[l] <- class.xi*class.eta[l]
+    class.eta[l] ~ dnorm(0,class.prec)
   }
   # finite population variance
-  fp.sd.tax <- sd(taxonomy)
+  fp.sd.class <- sd(classfx)
   fp.sd.hab <- sd(habitat)
   
   # half cauchy on random effects
-  tax.xi ~ dnorm(0,0.04)
-  tax.prec ~ dgamma(0.5,0.5)
+  class.xi ~ dnorm(0,0.001)
+  class.prec ~ dgamma(0.5,0.5)
   
-  tau ~ dgamma(0.1,0.1)
+  for (l in 1:n.order){
+    orderfx[l] <- order.xi*order.eta[l]
+    order.eta[l] ~ dnorm(0,order.prec)
+  }
+  # finite population variance
+  fp.sd.order <- sd(orderfx)
+  
+  # half cauchy on random effects
+  order.xi ~ dnorm(0,0.001)
+  order.prec ~ dgamma(0.5,0.5)
+  
+  for (l in 1:n.family){
+    familyfx[l] <- family.xi*family.eta[l]
+    family.eta[l] ~ dnorm(0,family.prec)
+  }
+  # finite population variance
+  fp.sd.family <- sd(familyfx)
+  
+  # half cauchy on random effects
+  family.xi ~ dnorm(0,0.001)
+  family.prec ~ dgamma(0.5,0.5)
+  
+  tau ~ dgamma(0.00001,0.00001)
   
 }
