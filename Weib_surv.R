9c86bb1ba058a542e602018069b323e184f2401c
Philipp Neubauer <neubauer.phil@gmail.com>
2016-10-29 23:13:36 +1300

    draft dataset and results

--- Weib_surv.R
+++ Weib_surv.R
@@ -9,7 +9,7 @@ model{
   a.time[i] ~ dweib(tau,mu[i])
   
   # linear predictor
-  mu[i] <- exp(logmean+betas[1:n.covs] %*% COVS[i,1:n.covs] + region[reg[i]] + habitat[hab[i]] + classfx[class[i]] + familyfx[family[i]] + orderfx[order[i]])
+  mu[i] <- exp(logmean+betas[1:n.covs] %*% COVS[i,1:n.covs] + region[reg[i]] + habitat[hab[i]] + classfx[class[i]] +  orderfx[order[i]])
   
   # cox-snell residuals - use pweib alone for predictions of P(t<T|X) i.e. for some predictor X what is the probability that a stock is assessed within T years
   CS[i] <- -log(1-pweib(ctime[i],tau,mu[i]))
@@ -26,10 +26,8 @@ model{
   ###
   
   for (j in 1:n.hab){
-    hab.pmu[j] <- exp(logmean+ habitat[j] + classfx.pred + orderfx.pred + familyfx.pred + regfx.pred)
-    for (t in tmin:tmax){
-       hab.pred[j,t] <- pweib(t,tau,hab.pmu[j])
-    }
+    hab.pmu[j] <- exp(logmean+ habitat[j] + classfx.pred + orderfx.pred  + regfx.pred)
+    
     habitat[j] <- hab.xi*hab.eta[j]
     hab.eta[j] ~ dnorm(0,hab.prec)
   }
@@ -40,10 +38,8 @@ model{
   hab.prec ~ dgamma(0.5,0.5)
   
   for (l in 1:n.class){
-    class.pmu[l] <- exp(logmean+ habfx.pred + classfx[l] + orderfx.pred + familyfx.pred + regfx.pred)
-    for (t in tmin:tmax){
-       class.pred[l,t] <- pweib(t,tau,class.pmu[l])
-    }
+    class.pmu[l] <- exp(logmean+ habfx.pred + classfx[l] + orderfx.pred + regfx.pred)
+    
     classfx[l] <- class.xi*class.eta[l]
     class.eta[l] ~ dnorm(0,class.prec)
   }
@@ -58,10 +54,8 @@ model{
   class.prec ~ dgamma(0.5,0.5)
   
   for (l in 1:n.order){
-    order.pmu[l] <- exp(logmean+ habfx.pred + classfx[classord[l]] + orderfx[l] + familyfx.pred + regfx.pred)
-    for (t in tmin:tmax){
-      order.pred[l,t] <- pweib(t,tau,order.pmu[l])
-    }
+    order.pmu[l] <- exp(logmean+ habfx.pred + classfx[classord[l]] + orderfx[l] + regfx.pred)
+    
     orderfx[l] <- order.xi*order.eta[l]
     order.eta[l] ~ dnorm(0,order.prec)
   }
@@ -74,28 +68,24 @@ model{
   order.xi ~ dnorm(0,0.001)
   order.prec ~ dgamma(0.5,0.5)
   
-  for (l in 1:n.family){
-    family.pmu[l] <- exp(logmean+ habfx.pred + classfx[classfam[l]] + familyfx[l] + orderfx[orderfam[l]] + regfx.pred)
-    for (t in tmin:tmax){
-      family.pred[l,t] <- pweib(t,tau,family.pmu[l])
-    }
-    familyfx[l] <- family.xi*family.eta[l]
-    family.eta[l] ~ dnorm(0,family.prec)
-  }
-  # finite population variance
-  fp.sd.family <- sd(familyfx)
-  
-  # half cauchy on random effects
-  familyfx.pred <- family.xi*family.eta.pred
-  family.eta.pred ~ dnorm(0,family.prec)
-  family.xi ~ dnorm(0,0.001)
-  family.prec ~ dgamma(0.5,0.5)
+  # for (l in 1:n.family){
+  #   family.pmu[l] <- exp(logmean+ habfx.pred + classfx[classfam[l]] + familyfx[l] + orderfx[orderfam[l]] + regfx.pred)
+  #  
+  #   familyfx[l] <- family.xi*family.eta[l]
+  #   family.eta[l] ~ dnorm(0,family.prec)
+  # }
+  # # finite population variance
+  # fp.sd.family <- sd(familyfx)
+  # 
+  # # half cauchy on random effects
+  # familyfx.pred <- family.xi*family.eta.pred
+  # family.eta.pred ~ dnorm(0,family.prec)
+  # family.xi ~ dnorm(0,0.001)
+  # family.prec ~ dgamma(0.5,0.5)
   
   for (l in 1:n.region){
-    region.pmu[l] <- exp(logmean+ habfx.pred + classfx.pred + region[l] + orderfx.pred + familyfx.pred)
-    for (t in tmin:tmax){
-      region.pred[l,t] <- pweib(t,tau,region.pmu[l])
-    }
+    region.pmu[l] <- exp(logmean+ habfx.pred + classfx.pred + region[l] + orderfx.pred )
+   
     region[l] <- region.xi*region.eta[l]
     region.eta[l] ~ dnorm(0,region.prec)
   }
